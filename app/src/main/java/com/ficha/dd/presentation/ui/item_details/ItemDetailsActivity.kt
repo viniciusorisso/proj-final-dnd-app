fedb3afea855a553667317b31da8e665556273c4
Vinícius Risso <viniciusorisso@gmail.com>
2023-02-05 18:03:33 -0300

    a

--- app/src/main/java/com/ficha/dd/presentation/ui/item_details/ItemDetailsActivity.kt
+++ app/src/main/java/com/ficha/dd/presentation/ui/item_details/ItemDetailsActivity.kt
@@ -1,7 +1,10 @@
 package com.ficha.dd.presentation.ui.item_details
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ficha.dd.MainActivity
 import com.ficha.dd.databinding.ItemDetailsBinding
 import com.ficha.dd.domain.model.CharacterSheet
 import com.ficha.dd.domain.model.Item
@@ -38,5 +41,14 @@ class ItemDetailsActivity : AppCompatActivity(){
         binding.detailsItemCost.text = viewModel.itemDetails.value?.url
         binding.detailsItemDescription.text = viewModel.itemDetails.value?.index
 //        binding.detailsItemName.text = viewModel.itemDetails.value?.name
+    }
+
+    companion object {
+        fun newIntent(context: Context, item: Item): Intent {
+            val intent: Intent = Intent(context, ItemDetailsActivity::class.java)
+            val bundle = intent.extras
+            bundle?.putParcelable("item", item)
+            return intent
+        }
     }
 }
--- app/src/main/java/com/ficha/dd/presentation/ui/items_list/ItemsListActivity.kt
+++ app/src/main/java/com/ficha/dd/presentation/ui/items_list/ItemsListActivity.kt
@@ -1,11 +1,15 @@
 package com.ficha.dd.presentation.ui.items_list
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.ficha.dd.MainActivity
 import com.ficha.dd.databinding.ItemsListBinding
+import com.ficha.dd.domain.model.CharacterSheet
+import com.ficha.dd.domain.model.Item
+import com.ficha.dd.presentation.ui.item_details.ItemDetailsActivity
 import com.ficha.dd.presentation.viewModel.ItemsViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -39,5 +43,10 @@ class ItemsListActivity : AppCompatActivity() {
         viewModel.allItems.observe(this) { list->
             adapter.setItems(list)
         }
+    }
+
+    private fun goToCharacterSheetDetails(item: Item) {
+        val intent = ItemDetailsActivity.newIntent(this ,item)
+        startActivity(intent)
     }
 }
